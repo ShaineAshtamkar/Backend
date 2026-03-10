42ba5896ac78b7b2b65c8bd640818f516da0ac0d
Shaine <shaineashtamkar099@gmail.com>
2026-03-10 21:55:15 +0200

    add protected recipe CRUD routes

--- recipes/controllers/recipeController.js
+++ recipes/controllers/recipeController.js
@@ -2,215 +2,362 @@ const { readRecipesFromFile, writeRecipesToFile } = require("../helpers/fileHelp
 const { v4: uuidv4 } = require("uuid");
 
 
-const getAllRecipes = async (req, res, next) => {
+// const getAllRecipes = async (req, res, next) => {
+//     try {
+//         let recipes = await readRecipesFromFile();
+
+//         const { difficulty, maxCookingTime, search } = req.query;
+
+//         if (difficulty) {
+//             recipes = recipes.filter(
+//                 (recipe) => recipe.difficulty.toLowerCase() === difficulty.toLowerCase()
+//             );
+//         }
+
+//         if (maxCookingTime) {
+//             const maxTime = Number(maxCookingTime);
+
+//             if (!Number.isNaN(maxTime)) {
+//                 recipes = recipes.filter((recipe) => recipe.cookingTime <= maxTime);
+//             }
+//         }
+
+//         if (search) {
+//             const searchTerm = search.toLowerCase();
+
+//             recipes = recipes.filter(
+//                 (recipe) =>
+//                     recipe.title.toLowerCase().includes(searchTerm) ||
+//                     recipe.description.toLowerCase().includes(searchTerm)
+//             );
+//         }
+
+//         res.status(200).json({
+//             error: false,
+//             count: recipes.length,
+//             data: recipes,
+//         });
+//     } catch (error) {
+//         next(error);
+//     }
+// };
+
+// const getRecipeById = async (req, res, next) => {
+//     try {
+//         const recipes = await readRecipesFromFile();
+//         const { id } = req.params;
+
+//         const recipe = recipes.find((recipe) => recipe.id === id);
+
+//         if (!recipe) {
+//             return res.status(404).json({
+//                 error: true,
+//                 message: "Recipe not found",
+//                 statusCode: 404,
+//             });
+//         }
+
+//         res.status(200).json({
+//             error: false,
+//             data: recipe,
+//         });
+//     } catch (error) {
+//         next(error);
+//     }
+// };
+
+
+// const getRecipeStats = async (req, res, next) => {
+//     try {
+//         const recipes = await readRecipesFromFile();
+
+//         const totalRecipes = recipes.length;
+
+//         const averageCookingTime =
+//             totalRecipes === 0
+//                 ? 0
+//                 : recipes.reduce((sum, recipe) => sum + recipe.cookingTime, 0) / totalRecipes;
+
+//         const recipesByDifficulty = {
+//             easy: 0,
+//             medium: 0,
+//             hard: 0,
+//         };
+
+//         recipes.forEach((recipe) => {
+//             if (recipesByDifficulty[recipe.difficulty] !== undefined) {
+//                 recipesByDifficulty[recipe.difficulty] += 1;
+//             }
+//         });
+
+//         const ingredientCounts = {};
+
+//         recipes.forEach((recipe) => {
+//             recipe.ingredients.forEach((ingredient) => {
+//                 const normalizedIngredient = ingredient.toLowerCase();
+//                 ingredientCounts[normalizedIngredient] =
+//                     (ingredientCounts[normalizedIngredient] || 0) + 1;
+//             });
+//         });
+
+//         const mostCommonIngredients = [];
+
+//         for (const ingredient in ingredientCounts) {
+//             mostCommonIngredients.push({
+//                 ingredient: ingredient,
+//                 count: ingredientCounts[ingredient],
+//             });
+//         }
+
+//         mostCommonIngredients.sort((a, b) => b.count - a.count);
+
+//         const topFiveIngredients = mostCommonIngredients.slice(0, 5);
+
+//         res.status(200).json({
+//             error: false,
+//             data: {
+//                 totalRecipes,
+//                 averageCookingTime,
+//                 recipesByDifficulty,
+//                 topFiveIngredients,
+//             },
+//         });
+//     } catch (error) {
+//         next(error);
+//     }
+// };
+
+
+// const createRecipe = async (req, res, next) => {
+//     try {
+//         const recipes = await readRecipesFromFile();
+
+//         const newRecipe = {
+//             id: uuidv4(),
+//             ...req.body,
+//             createdAt: new Date().toISOString(),
+//         };
+
+//         recipes.push(newRecipe);
+
+//         await writeRecipesToFile(recipes);
+
+//         res.status(201).json({
+//             error: false,
+//             data: newRecipe,
+//         });
+//     } catch (error) {
+//         next(error);
+//     }
+// };
+
+
+// const updateRecipe = async (req, res, next) => {
+//     try {
+//         const recipes = await readRecipesFromFile();
+//         const { id } = req.params;
+
+//         const recipeIndex = recipes.findIndex((recipe) => recipe.id === id);
+
+//         if (recipeIndex === -1) {
+//             return res.status(404).json({
+//                 error: true,
+//                 message: "Recipe not found",
+//                 statusCode: 404,
+//             });
+//         }
+
+//         const updatedRecipe = {
+//             ...recipes[recipeIndex],
+//             ...req.body,
+//         };
+
+//         recipes[recipeIndex] = updatedRecipe;
+
+//         await writeRecipesToFile(recipes);
+
+//         res.status(200).json({
+//             error: false,
+//             data: updatedRecipe,
+//         });
+//     } catch (error) {
+//         next(error);
+//     }
+// };
+
+// const deleteRecipe = async (req, res, next) => {
+//     try {
+//         const recipes = await readRecipesFromFile();
+//         const { id } = req.params;
+
+//         const recipeIndex = recipes.findIndex((recipe) => recipe.id === id);
+
+//         if (recipeIndex === -1) {
+//             return res.status(404).json({
+//                 error: true,
+//                 message: "Recipe not found",
+//                 statusCode: 404,
+//             });
+//         }
+
+//         recipes.splice(recipeIndex, 1);
+
+//         await writeRecipesToFile(recipes);
+
+//         res.status(204).send();
+//     } catch (error) {
+//         next(error);
+//     }
+// };
+
+
+const { Recipe } = require('../db/models');
+
+const createRecipe = async (req, res) => {
     try {
-        let recipes = await readRecipesFromFile();
+        const {
+            title,
+            description,
+            ingredients,
+            instructions,
+            cookingTime,
+            servings,
+            difficulty,
+            isPublic
+        } = req.body;
 
-        const { difficulty, maxCookingTime, search } = req.query;
-
-        if (difficulty) {
-            recipes = recipes.filter(
-                (recipe) => recipe.difficulty.toLowerCase() === difficulty.toLowerCase()
-            );
+        if (
+            !title ||
+            !ingredients ||
+            !instructions ||
+            !cookingTime ||
+            !servings ||
+            !difficulty
+        ) {
+            return res.status(400).json({
+                success: false,
+                message: 'Missing required recipe fields'
+            });
         }
 
-        if (maxCookingTime) {
-            const maxTime = Number(maxCookingTime);
+        const recipe = await Recipe.create({
+            title,
+            description,
+            ingredients,
+            instructions,
+            cookingTime,
+            servings,
+            difficulty,
+            isPublic: isPublic !== undefined ? isPublic : true,
+            userId: req.user.userId
+        });
 
-            if (!Number.isNaN(maxTime)) {
-                recipes = recipes.filter((recipe) => recipe.cookingTime <= maxTime);
-            }
-        }
-
-        if (search) {
-            const searchTerm = search.toLowerCase();
-
-            recipes = recipes.filter(
-                (recipe) =>
-                    recipe.title.toLowerCase().includes(searchTerm) ||
-                    recipe.description.toLowerCase().includes(searchTerm)
-            );
-        }
-
-        res.status(200).json({
-            error: false,
-            count: recipes.length,
-            data: recipes,
+        res.status(201).json({
+            success: true,
+            message: 'Recipe created successfully',
+            recipe
         });
     } catch (error) {
-        next(error);
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
     }
 };
 
-const getRecipeById = async (req, res, next) => {
+const getMyRecipes = async (req, res) => {
     try {
-        const recipes = await readRecipesFromFile();
-        const { id } = req.params;
+        const recipes = await Recipe.findAll({
+            where: { userId: req.user.userId },
+            order: [['createdAt', 'DESC']]
+        });
 
-        const recipe = recipes.find((recipe) => recipe.id === id);
+        res.status(200).json({
+            success: true,
+            count: recipes.length,
+            recipes
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+};
+
+const updateRecipe = async (req, res) => {
+    try {
+        const recipe = await Recipe.findByPk(req.params.id);
 
         if (!recipe) {
             return res.status(404).json({
-                error: true,
-                message: "Recipe not found",
-                statusCode: 404,
+                success: false,
+                message: 'Recipe not found'
             });
         }
 
-        res.status(200).json({
-            error: false,
-            data: recipe,
-        });
-    } catch (error) {
-        next(error);
-    }
-};
-
-
-const getRecipeStats = async (req, res, next) => {
-    try {
-        const recipes = await readRecipesFromFile();
-
-        const totalRecipes = recipes.length;
-
-        const averageCookingTime =
-            totalRecipes === 0
-                ? 0
-                : recipes.reduce((sum, recipe) => sum + recipe.cookingTime, 0) / totalRecipes;
-
-        const recipesByDifficulty = {
-            easy: 0,
-            medium: 0,
-            hard: 0,
-        };
-
-        recipes.forEach((recipe) => {
-            if (recipesByDifficulty[recipe.difficulty] !== undefined) {
-                recipesByDifficulty[recipe.difficulty] += 1;
-            }
-        });
-
-        const ingredientCounts = {};
-
-        recipes.forEach((recipe) => {
-            recipe.ingredients.forEach((ingredient) => {
-                const normalizedIngredient = ingredient.toLowerCase();
-                ingredientCounts[normalizedIngredient] =
-                    (ingredientCounts[normalizedIngredient] || 0) + 1;
-            });
-        });
-
-        const mostCommonIngredients = [];
-
-        for (const ingredient in ingredientCounts) {
-            mostCommonIngredients.push({
-                ingredient: ingredient,
-                count: ingredientCounts[ingredient],
+        if (recipe.userId !== req.user.userId) {
+            return res.status(403).json({
+                success: false,
+                message: 'You can only update your own recipes'
             });
         }
 
-        mostCommonIngredients.sort((a, b) => b.count - a.count);
-
-        const topFiveIngredients = mostCommonIngredients.slice(0, 5);
+        await recipe.update(req.body);
 
         res.status(200).json({
-            error: false,
-            data: {
-                totalRecipes,
-                averageCookingTime,
-                recipesByDifficulty,
-                topFiveIngredients,
-            },
+            success: true,
+            message: 'Recipe updated successfully',
+            recipe
         });
     } catch (error) {
-        next(error);
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
     }
 };
 
-
-const createRecipe = async (req, res, next) => {
+const deleteRecipe = async (req, res) => {
     try {
-        const recipes = await readRecipesFromFile();
+        const recipe = await Recipe.findByPk(req.params.id);
 
-        const newRecipe = {
-            id: uuidv4(),
-            ...req.body,
-            createdAt: new Date().toISOString(),
-        };
-
-        recipes.push(newRecipe);
-
-        await writeRecipesToFile(recipes);
-
-        res.status(201).json({
-            error: false,
-            data: newRecipe,
-        });
-    } catch (error) {
-        next(error);
-    }
-};
-
-
-const updateRecipe = async (req, res, next) => {
-    try {
-        const recipes = await readRecipesFromFile();
-        const { id } = req.params;
-
-        const recipeIndex = recipes.findIndex((recipe) => recipe.id === id);
-
-        if (recipeIndex === -1) {
+        if (!recipe) {
             return res.status(404).json({
-                error: true,
-                message: "Recipe not found",
-                statusCode: 404,
+                success: false,
+                message: 'Recipe not found'
             });
         }
 
-        const updatedRecipe = {
-            ...recipes[recipeIndex],
-            ...req.body,
-        };
+        if (recipe.userId !== req.user.userId) {
+            return res.status(403).json({
+                success: false,
+                message: 'You can only delete your own recipes'
+            });
+        }
 
-        recipes[recipeIndex] = updatedRecipe;
-
-        await writeRecipesToFile(recipes);
+        await recipe.destroy();
 
         res.status(200).json({
-            error: false,
-            data: updatedRecipe,
+            success: true,
+            message: 'Recipe deleted successfully'
         });
     } catch (error) {
-        next(error);
-    }
-};
-
-const deleteRecipe = async (req, res, next) => {
-    try {
-        const recipes = await readRecipesFromFile();
-        const { id } = req.params;
-
-        const recipeIndex = recipes.findIndex((recipe) => recipe.id === id);
-
-        if (recipeIndex === -1) {
-            return res.status(404).json({
-                error: true,
-                message: "Recipe not found",
-                statusCode: 404,
-            });
-        }
-
-        recipes.splice(recipeIndex, 1);
-
-        await writeRecipesToFile(recipes);
-
-        res.status(204).send();
-    } catch (error) {
-        next(error);
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
     }
 };
 
 module.exports = {
-    getAllRecipes, getRecipeById, getRecipeStats, createRecipe, updateRecipe, deleteRecipe,
-
+    createRecipe,
+    getMyRecipes,
+    updateRecipe,
+    deleteRecipe
 };
+
+// module.exports = {
+//     getAllRecipes, getRecipeById, getRecipeStats, createRecipe, updateRecipe, deleteRecipe,
+
+// };
--- recipes/routes/recipeRoutes.js
+++ recipes/routes/recipeRoutes.js
@@ -1,23 +1,40 @@
 const express = require("express");
-const router = express.Router();
-const { getAllRecipes,
-    getRecipeById,
-    getRecipeStats,
+const authMiddleware = require('../middleware/authMiddleware');
+
+
+// const { getAllRecipes,
+//     getRecipeById,
+//     getRecipeStats,
+//     createRecipe,
+//     updateRecipe,
+//     deleteRecipe
+// } = require("../controllers/recipeController");
+
+const {
     createRecipe,
+    getMyRecipes,
     updateRecipe,
     deleteRecipe
-} = require("../controllers/recipeController");
+} = require('../controllers/recipeController');
+const router = express.Router();
+
 
 const validateRecipe = require("../middleware/validateRecipe");
 
 
-router.get("/", getAllRecipes);
-router.get("/stats", getRecipeStats);
-router.get("/:id", getRecipeById);
+// router.get("/", getAllRecipes);
+// router.get("/stats", getRecipeStats);
+// router.get("/:id", getRecipeById);
 
-router.post("/", validateRecipe, createRecipe);
-router.put("/:id", validateRecipe, updateRecipe);
-router.delete("/:id", deleteRecipe);
+// router.post("/", validateRecipe, createRecipe);
+// router.put("/:id", validateRecipe, updateRecipe);
+// router.delete("/:id", deleteRecipe);
+
+router.post('/', authMiddleware, createRecipe);
+router.get('/my-recipes', authMiddleware, getMyRecipes);
+router.put('/:id', authMiddleware, updateRecipe);
+router.delete('/:id', authMiddleware, deleteRecipe);
+
 
 
 module.exports = router;
