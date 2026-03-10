33f4bd4e201828eb0e0354a41d1378db2003d106
Shaine <shaineashtamkar099@gmail.com>
2026-03-10 13:48:02 +0200

    add raw SQL queries using sequelize.query

--- recipes/app.js
+++ recipes/app.js
@@ -5,7 +5,7 @@ const errorHandler = require("./middleware/errorHandler");
 
 
 const app = express();
-
+const { sequelize } = require('./db/models');
 
 // built-in middleware
 app.use(express.json());
@@ -22,6 +22,50 @@ app.get("/", (req, res) => {
 
 // recipe routes
 app.use("/api/recipes", recipeRoutes);
+
+app.get('/users', async (req, res) => {
+    try {
+        const [results, metadata] = await sequelize.query('SELECT * FROM "Users"');
+
+        res.status(200).json({
+            success: true,
+            users: results
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+});
+
+app.post('/users', async (req, res) => {
+    try {
+        const { username, email, password, firstName, lastName } = req.body;
+
+        const query = `
+      INSERT INTO "Users" (id, username, email, password, "firstName", "lastName", "createdAt", "updatedAt")
+      VALUES (uuid_generate_v4(), :username, :email, :password, :firstName, :lastName, NOW(), NOW())
+      RETURNING *
+    `;
+
+        const [results] = await sequelize.query(query, {
+            replacements: { username, email, password, firstName, lastName }
+        });
+
+        res.status(201).json({
+            success: true,
+            user: results[0]
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+});
 
 // 404 handler for unknown routes
 app.use((req, res) => {
