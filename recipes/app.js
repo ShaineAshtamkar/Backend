f72fe4d8b1ddb1b00a51dc8786e102de60f0d099
Shaine <shaineashtamkar099@gmail.com>
2026-03-10 14:44:58 +0200

    remove temporary SQL and ORM test routes

--- recipes/app.js
+++ recipes/app.js
@@ -6,6 +6,7 @@ const errorHandler = require("./middleware/errorHandler");
 
 const app = express();
 const { sequelize } = require('./db/models');
+const { User } = require('./db/models');
 
 // built-in middleware
 app.use(express.json());
@@ -23,49 +24,6 @@ app.get("/", (req, res) => {
 // recipe routes
 app.use("/api/recipes", recipeRoutes);
 
-app.get('/users', async (req, res) => {
-    try {
-        const [results, metadata] = await sequelize.query('SELECT * FROM "Users"');
-
-        res.status(200).json({
-            success: true,
-            users: results
-        });
-
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            message: error.message
-        });
-    }
-});
-
-app.post('/users', async (req, res) => {
-    try {
-        const { username, email, password, firstName, lastName } = req.body;
-
-        const query = `
-      INSERT INTO "Users" (id, username, email, password, "firstName", "lastName", "createdAt", "updatedAt")
-      VALUES (uuid_generate_v4(), :username, :email, :password, :firstName, :lastName, NOW(), NOW())
-      RETURNING *
-    `;
-
-        const [results] = await sequelize.query(query, {
-            replacements: { username, email, password, firstName, lastName }
-        });
-
-        res.status(201).json({
-            success: true,
-            user: results[0]
-        });
-
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            message: error.message
-        });
-    }
-});
 
 // 404 handler for unknown routes
 app.use((req, res) => {
---
+++ recipes/db/models/user.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
+            allowNull: false
+        },
+        username: {
+            type: DataTypes.STRING(30),
+            allowNull: false,
+            unique: true,
+            validate: {
+                notEmpty: true,
+                len: [3, 30]
+            }
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    }, {
+        tableName: 'Users',
+        timestamps: true
+    });
+
+    return User;
+};
